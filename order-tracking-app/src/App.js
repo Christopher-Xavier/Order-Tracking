7df1be35826fca6c86dd703bb89128f4f4e21d06
username <useremail>
2025-04-28 17:14:43 -0700

    created react app

---
+++ order-tracking-app/src/App.js
@@ -0,0 +1,10 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+      return (
+            <div>
+                  <h1>Business Order Tracking, Welcome!</h1>       
+                   </div>);
+}
+export default App;
